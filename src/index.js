6ef25c5f5a067cdaa047f5e568ef9245df995a5b
Changsoo Jung <cjung2020@gmail.com>
2022-12-13 13:41:39 -0700

    update packagge.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
 render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ThemeProvider>
       <App />
     </ThemeProvider>
